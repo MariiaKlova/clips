b25ba4bb15f63833ad3f7d6ebee92ca100d1a89f
mklova <mklova@techmatesgroup.com>
2025-05-20 15:21:56 +0300

    modal

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  standalone: false,
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent implements OnInit {
+  @Input() modalID = ''
+
+  constructor(
+    public modal:ModalService, 
+    public el: ElementRef
+  ){
+    
+   }
+
+  ngOnInit(): void{
+    document.body.appendChild(this.el.nativeElement)
+  }
+
+  closeModal(){
+   this.modal.toggleModal(this.modalID);
+  }
+
+
+}
